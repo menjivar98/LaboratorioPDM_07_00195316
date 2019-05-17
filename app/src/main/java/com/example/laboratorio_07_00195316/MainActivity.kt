fb1a10758f9691d74afb2919b2c24e14b50399fc
Fernando Menjivar <00195316@uca.edu.sv>
2019-05-17 07:29:39 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/laboratorio_07_00195316/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.laboratorio_07_00195316
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
