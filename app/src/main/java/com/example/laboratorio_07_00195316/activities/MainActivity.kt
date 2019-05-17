a4525e99fa82873fe50f464e6f18d7253d8f60f8
Fernando Menjivar <00195316@uca.edu.sv>
2019-05-17 09:04:18 -0600

    Se termino

--- app/src/main/java/com/example/laboratorio_07_00195316/activities/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio_07_00195316/activities/MainActivity.kt
@@ -2,12 +2,49 @@ package com.example.laboratorio_07_00195316.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.laboratorio_07_00195316.R
+import com.example.laboratorio_07_00195316.database.entities.GithubRepo
+import com.example.laboratorio_07_00195316.viewmodels.GithubRepoViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var viewModel: GithubRepoViewModel
+    var cont =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bind()
+    }
+
+    private fun bind(){
+
+        viewModel = ViewModelProviders.of(this)
+            .get(GithubRepoViewModel::class.java)
+
+        viewModel.getRepoList().observe(this, Observer {
+
+            Log.d("Repos", "------------------------------")
+
+            for (i in 0 until it.size){
+                Log.d("Repos", it[i].name)
+            }
+
+
+        })
+
+        btn_action.setOnClickListener {
+            viewModel.insert(GithubRepo("Repositorio #" + cont))
+            cont++
+
+        }
+
     }
 }
